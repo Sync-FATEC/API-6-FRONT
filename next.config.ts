56ff2b19ba8c2f1becba3f511b95b535a0621462
Ana Laura <analauramoratelli203@gmail.com>
2026-05-25 17:03:20 -0300

    feat: #SCRUM-110 adicionando rota do backend

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    if (!isProd) return [];
+
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "http://asg-backend-alb-85114170.us-east-1.elb.amazonaws.com/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
